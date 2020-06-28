bee060f710439dd29754d43faece0e189737e40e
Jeremy Aube <jeremyaube24@gmail.com>
2020-06-28 17:29:26 -0400

    Moisture sensor code
    Added code to read moisture sensor level and start the water pump based on the voltage

--- plant-water-automation/src/main.c
+++ plant-water-automation/src/main.c
@@ -1,18 +1,33 @@
-#include "water_pump.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
 #include "esp_log.h"
-
-#define WATER_PUMP_IO 15
+#include "esp_adc_cal.h"
+#include "water_pump.h"
 
 void app_main(void)
 {
-    struct WaterPump water_pump = init_water_pump(WATER_PUMP_IO, 2500);
+    adc1_config_width(ADC_WIDTH_12Bit);
+    adc1_config_channel_atten(ADC1_GPIO34_CHANNEL, ADC_ATTEN_11db);
 
-    start_water_pump(&water_pump);
-    start_water_pump(&water_pump);
-    start_water_pump(&water_pump);
+    esp_adc_cal_characteristics_t *adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_11db, ADC_WIDTH_12Bit, ESP_ADC_CAL_VAL_DEFAULT_VREF, adc_chars);
+
+    struct WaterPump water_pump = init_water_pump(15, 500);
 
     while (1)
     {
-        ESP_LOGW("debug", "water_pump");
+        int value = adc1_get_raw(ADC1_GPIO34_CHANNEL);
+        int voltage = esp_adc_cal_raw_to_voltage(value, adc_chars);
+
+        if (voltage < 1500)
+        {
+            start_water_pump(&water_pump);
+            printf("start water");
+        }
+
+        ESP_LOGW("adc1", "value %d", voltage);
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
