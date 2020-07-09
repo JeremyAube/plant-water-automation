1a04b6c20e4d4a043c856d485cc5cf4bb00ac1da
Jeremy Aube <jeremyaube24@gmail.com>
2020-07-08 21:34:04 -0400

    added code in wifi module. Still neeed to test

--- esp32/src/wifi.c
+++ esp32/src/wifi.c
@@ -1,6 +1,14 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 #include "wifi.h"
 #include "esp_wifi.h"
+#include "esp_log.h"
 #include "wifi_config.h"
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+static const char *TAG = "wifi station";
+static EventGroupHandle_t s_wifi_event_group;
 
 void init_wifi()
 {
@@ -20,4 +28,25 @@ void init_wifi()
     esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
     esp_wifi_set_mode(WIFI_MODE_STA);
     esp_wifi_start();
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "connected to wifi!");
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(TAG, "Failed to connect to wifi!");
+    }
+    else
+    {
+        ESP_LOGI(TAG, "UNEXPECTED EVENT");
+    }
 }
--- esp32/src/wifi_config_example.h
+++ esp32/src/wifi_config_example.h
@@ -1,2 +1,4 @@
+// Rename this file to wifi_config.h and enter the correct SSID and Password
+
 #define WIFI_CONFIG_SSID "Access Point SSID"
 #define WIFI_CONFIG_PASSWORD "Access Point Password"
